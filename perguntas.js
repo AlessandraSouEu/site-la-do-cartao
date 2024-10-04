5146aa1ff0927f4edcf925f0605969d5cd08d969
meu <142503407+AlessandraSouEu@users.noreply.github.com>
2024-10-04 08:32:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'OSNF',
+    'Quantos anos Thiago passou nas mãos do Deus da Morte?',
+    '51 anos'
+)
+
+criaCartao(
+    'OPD',
+    'Qual o ritual utilizado por Kaiser antes de sua morte?',
+    'Cinerária'
+)
+
+criaCartao(
+    'OPC',
+    '"Eu vou até o inferno pra matar todos vocês! Um por um!" Quem disse isso?',
+    'Joui Jouki (vulgo gostoso)'
+)
+
+criaCartao(
+    'Spinoff',
+    'Qual é a única fobia de rubens?' ,
+    'Hidrofobia: Rubens tem medo de se afogar. Isso foi mostrado no episódio 7 de Desconjuração quando um Perturbado de energia lhe mostrou seu maior medo em uma visão.'
+)
+
+criaCartao(
+    'geral',
+    'Qual a temporada de ordem com mais episodios?',
+    'Ordem Paranormal Desconjuração 20 eps'
+)
+
+criaCartao(
+    'OPQ',
+    '"No man, im not Super-Man, Im Super-Mexican!" Quem disse isso?',
+    'Luis Miguel Kennedy'
+)
